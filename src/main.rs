bf280b7ec793129ee386f39e22188e59878ca76b
MagTheDev <jtulek@at-rt.com>
2022-06-19 21:29:37 +0200

    Add basic file reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{path::PathBuf, fs::OpenOptions, io::{Read, BufReader}};
+
+use anyhow::Context;
+use structopt::StructOpt;
+
 fn main() {
-    println!("Hello, world!");
+
+    let args = Args::from_args();
+
+    let file = OpenOptions::new()
+        .read(true)
+        .open(args.input)
+        .context("Unable to read file").unwrap();
+
+    let mut reader = BufReader::new(file);
+    let mut buffer = Vec::new();
+
+    reader.read_to_end(&mut buffer).context("Unable to read file contents to buffer").unwrap();
+
+    
+
+}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "HeX Edit", about = "A simple program that lets you edit the binary of a file")]
+struct Args {
+    #[structopt(parse(from_os_str))]
+    input: PathBuf
 }
